526f0d24f38125295f53ea3b6c128e29897d70c0
franbarbera <francobarberabarrio@gmail.com>
2025-10-24 21:48:13 -0300

    primer commit

---
+++ operaciones.js
@@ -0,0 +1,28 @@
+
+const fs = require("fs");
+
+const registrar = (nombre, edad, tipo, color, enfermedad) => {
+    const citas = JSON.parse(fs.readFileSync("citas.json", "utf8"));
+
+    const nuevaCita = {
+        nombre,
+        edad,
+        tipo,
+        color,
+        enfermedad,
+    };
+
+    citas.push(nuevaCita);
+    fs.writeFileSync("citas.json", JSON.stringify(citas, null, 2));
+
+    console.log("Cita registrada con éxito!");
+};
+
+
+const leer = () => {
+    const citas = JSON.parse(fs.readFileSync("citas.json", "utf8"));
+    console.log("📋 Listado de citas registradas:");
+    console.log(citas);
+};
+
+module.exports = { registrar, leer };
